d1320f8858f2a8b12d541072cef07d04ccb008e0
Unmesh Dabhade <unmesh.dabhade@gmail.com>
2021-08-27 10:58:35 +0530

    Bug Fix

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8;
 import "./DappToken.sol";
 
 contract DappTokenSale {
-    address          admin;
+    address payable admin;
     DappToken public tokenContract;
     uint256   public tokenPrice; // in wei
     uint256   public tokensSold;
@@ -11,7 +11,7 @@ contract DappTokenSale {
     event Sell(address _buyer, uint256 _amount);
 
     constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
-        admin = msg.sender;
+        admin = payable(msg.sender);
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
@@ -22,18 +22,18 @@ contract DappTokenSale {
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
         tokensSold += _numberOfTokens;
 
-        Sell(msg.sender, _numberOfTokens);
+        emit Sell(msg.sender, _numberOfTokens);
     }
 
 
     function endSale() public {
         require(msg.sender == admin);
-        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
 
         selfdestruct(admin);
     }
